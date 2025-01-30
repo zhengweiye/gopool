5355de15c8b286989a8cf241324de40a297dff1e
郑伟业 <286128576@qq.com>
2025-01-30 23:19:45 +0800

    测试用例

---
+++ task_future_test.go
@@ -0,0 +1,71 @@
+package gopool
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func Test_Task_Future(t *testing.T) {
+	ctx, _ := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	workerPool := NewPool(100, 20, 100, ctx, wg)
+	var futureList []chan Future
+	for i := 0; i < 5; i++ {
+		future := make(chan Future)
+		workerPool.ExecTaskFuture(JobFuture{
+			JobName: "测试",
+			JobFunc: deal,
+			JobParam: map[string]any{
+				"id": i,
+			},
+			Future: future,
+		})
+		futureList = append(futureList, future)
+		//TODO 串行执行
+		/*result := <-future
+		if result.Error != nil {
+			panic(result.Error)
+		}
+		fmt.Printf("执行：id=%d, result=%v\n", i, result.Result)*/
+	}
+
+	//TODO 并行执行
+	for _, future := range futureList {
+		result := <-future
+		//TODO 感觉这里抛异常不合适，影响其它chan
+		if result.Error != nil {
+			panic(result.Error)
+		}
+		fmt.Printf("result=%v\n", result.Result)
+
+		//TODO 内部已经关闭了
+		//close(future)
+	}
+}
+
+func deal(workerId int, jobName string, param map[string]any, future chan Future) {
+	var name string
+	defer func() {
+		if err := recover(); err != nil {
+			future <- Future{
+				Error: fmt.Errorf("%v", err),
+			}
+		} else {
+			future <- Future{
+				Result: name,
+			}
+		}
+	}()
+
+	id := param["id"].(int)
+	/*	if id == 3 {
+		panic("测试异常")
+	}*/
+	fmt.Println("id=", id, ", 开始执行")
+	time.Sleep(10 * time.Second)
+	fmt.Println("id=", id, ", 结束执行")
+	name = fmt.Sprintf("%d", id+10)
+}
---
+++ task_test.go
@@ -0,0 +1,43 @@
+package gopool
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+func Test_Task(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	workerPool := NewPool(100, 20, 100, ctx, wg)
+
+	for i := 0; i < 5; i++ {
+		workerPool.ExecTask(Job{
+			JobName: fmt.Sprintf("作业名称%d", i),
+			JobFunc: myJob,
+			JobParam: map[string]any{
+				"name":    fmt.Sprintf("姓名%d", i),
+				"address": fmt.Sprintf("地址%d", i),
+			},
+		})
+	}
+
+	time.Sleep(5 * time.Second)
+	cancel()
+	//pool.Shutdown()
+
+	wg.Wait()
+	fmt.Println("结束.....")
+}
+
+func myJob(workerId int, jobName string, param map[string]any) (err error) {
+	//fmt.Println("before: jobName=", "线程Id=", workerId, jobName, ", 参数=", param)
+	rand.Seed(time.Now().UnixNano())
+	t := rand.Intn(10)
+	time.Sleep(time.Duration(int64(time.Second) * int64(t)))
+	fmt.Println("after: jobName=", "线程Id=", workerId, jobName, ", 参数=", param)
+	return
+}
